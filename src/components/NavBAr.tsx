90556c71f83c4b4a2fbaeca288905f131b5bb509
Romik3df <michal.chamilla@vdp.sk>
2024-10-15 12:37:02 +0200

    icons add

--- src/components/NavBAr.tsx
+++ src/components/NavBAr.tsx
@@ -1,18 +1,31 @@
-// src/NavBar/page.tsx
+// src/components/NavBar.tsx
 
+"use client"
 
-
-import Typography from '@mui/material/FormLabel';
-import Container from '@mui/material/FormLabel';
-
-export const metadata = {title: "NavBar| Zochova"};
-
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function NavBar() {
-  return (
-        <Container>
-            <Typography>NavBar</Typography>
-        </Container>
+  const [value, setValue] = React.useState(0);
 
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
   );
 }
